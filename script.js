980755608c5b347d2ea1438d4dcb07d6581915b6
hbdsuya <venusnezha@gmail.com>
2025-08-28 17:40:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ function initengahan(){
   new TypeIt("#pesan5", {
   strings: ["" + vketik5], startDelay: 1, speed: 52, cursor: true, waitUntilVisible: true, lifelike: true,
   afterComplete: function(){
-    pesan5.innerHTML=vketik5 + " ><";setTimeout(aktipesan6,800);
+    pesan5.innerHTML=vketik5 ;setTimeout(aktipesan6,800);
   },}).go();
   }
   vketik6=pesan6.innerHTML;pesan6.innerHTML="";
@@ -147,7 +147,6 @@ function initengahan(){
   ftganti=4;ftmuncul();
   
   new TypeIt("#pesan8", {
-  strings: [vketik81], startDelay: 20, speed: 45, cursor: true, deleteSpeed: 30, breakLines: false, waitUntilVisible: true, lifelike: true,
   afterComplete: function(){
     setTimeout(function() {
       new TypeIt("#pesan9", {
